d3363d8a2c2653fb1d7876e5f2be4d6720a7e33b
byjusmx3 <monica.castaneda@byjusfutureschool.com>
2022-05-12 09:51:08 -0500

    copy

--- Walkers.py
+++ Walkers.py
@@ -1,24 +1,24 @@
 import cv2
 import numpy as np
 
-# Create our body classifier
+# Crea nuestro body classifier
 body_classifier = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 
-# Initiate video capture for video file
+# Inicializa video capture para el archivo de video
 cap = cv2.VideoCapture('walking.avi')
 
-# Loop once video is successfully loaded
+# Inicia el bucle cuando el video se haya cargado correctamente
 while True:
     
-    # Read first frame
+    # Lee el primer cuadro
     ret, frame = cap.read()
     #frame = cv2.resize(frame, None,fx=0.5, fy=0.5, interpolation = cv2.INTER_LINEAR)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Pass frame to our body classifier
+    # Pasa el cuadro a nuestro body classifier
     bodies = body_classifier.detectMultiScale(gray, 1.2, 3)
     
-    # Extract bounding boxes for any bodies identified
+    # Extrae los cuadros delimitadores de los cuerpos identificados
     for (x,y,w,h) in bodies:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
         cv2.imshow('Pedestrians', frame)
